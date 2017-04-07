2e83416ba54e12244ac7803c1c69b58fdfda2732
Naomie <sonny.naomie@gmail.com>
2017-04-07 11:49:30 +0200

    Score + mort du bloquement du mouvement du chat

--- Assets/Script/DestroyCroquette.cs
+++ Assets/Script/DestroyCroquette.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyCroquette : MonoBehaviour {
-	
+
+	public Score score;
+
 	void OnCollisionEnter (Collision col)
 	{
 		if(col.gameObject.tag == "Player")
 		{
-			Score.score  += 100;
+			score.score  += 1;
 			//PlayerPrefs.SetIn t("Score", score.score);
 			Destroy(gameObject,1.2f);
 		}
